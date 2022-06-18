a97a2612ec7301c33699adca866112ed6bd56129
harvey <harveygleeson@gmail.com>
2022-06-18 14:55:17 +0100

    Add dummy modal onclick

---
+++ frontend/src/Components/Modals/Modal.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Modal.css";
+
+const Modal = () => {
+  return (
+    <div className="modal">
+      <div class="p-4 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md sm:p-6 lg:p-8 dark:bg-gray-800 dark:border-gray-700">
+        Pop Up Window
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
