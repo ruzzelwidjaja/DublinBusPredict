5cb288ea4997bd568cbbf777e0ac8c07a211aa13
harvey <harveygleeson@gmail.com>
2022-06-23 18:00:01 +0100

    Add loading screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
-import "./App.css";
 import React, { useState, useEffect } from "react";
 import { useJsApiLoader } from "@react-google-maps/api";
+import ReactLoading from "react-loading";
+import "./App.css";
 import Map from "./Components/Map";
 import Navbar from "./Components/Navbar";
 import Modal from "./Components/Modals/Modal";
@@ -46,7 +47,16 @@ const App = () => {
 
   // If map has not loaded display loading..
   if (!isLoaded) {
-    return <>Loading...</>;
+    return (
+      <div className="absolute h-full w-full bg-zinc-900 ">
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          <ReactLoading type={"spin"} color="#475569" />
+        </div>
+        <div className="text-center absolute top-2/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          <p className="text-slate-500">Loading..</p>
+        </div>
+      </div>
+    );
   }
 
   return (
